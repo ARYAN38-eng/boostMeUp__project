a176c645b640268a297007a4b733f86a25ad767f
ARYAN38-eng <aryanraina2021@gmail.com>
2025-06-25 13:52:24 +0530

    responsiveness improved

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,62 +1,132 @@
-"use client"
-import React, { useState, useRef,useEffect } from 'react'
-import { useSession, signOut } from "next-auth/react"
-import Link from 'next/link'
-import Image from "next/image"
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-  const { data: session } = useSession()
-  const [showdropdown, setShowdropdown] = useState(false)
-  const dropdownRef = useRef(null)
+  const { data: session } = useSession();
+  const [showdropdown, setShowdropdown] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
     function handleClickOutside(event) {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setShowdropdown(false)
+        setShowdropdown(false);
       }
     }
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
-  }, [])
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   return (
-    <nav className='bg-gray-900 shadow-xl shadow-white text-white flex justify-between items-center px-4 md:h-16'>
-
-      <Link className="logo font-bold text-lg flex justify-center items-center" href={"/"}>
-        <Image className='invertImg' src="/output-onlinegiftools.gif" width={44} height={44} alt="" />
-        <span className='text-xl md:text-base ml-4 my-3 md:my-0'>BoostMeUp!</span>
+    <nav className="bg-gray-900 shadow-xl shadow-white text-white flex justify-between items-center px-4 md:h-16">
+      <Link
+        className="logo font-bold text-lg flex justify-center items-center"
+        href={"/"}
+      >
+        <Image
+          className="invertImg"
+          src="/output-onlinegiftools.gif"
+          width={44}
+          height={44}
+          alt=""
+        />
+        <span className="text-xl md:text-base ml-4 my-3 md:my-0">
+          BoostMeUp!
+        </span>
       </Link>
 
+      <div className="relative flex justify-center items-center ml-6 sm:ml-auto md:block gap-2 sm:gap-4">
+        {session && (
+          <>
+            <button
+              onClick={() => setShowdropdown(!showdropdown)}
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+            >
+              <span className="hidden sm:inline">{session.user.email}</span>
 
-      <div className='relative flex justify-center items-center  md:block gap-4'>
-        {session && <>
-          <button onClick={() => setShowdropdown(!showdropdown)} 
-            id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">{session.user.email}<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-            </svg>
+              <span className="inline sm:hidden">Options</span>
+              <svg
+                className="w-2.5 h-2.5 ms-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            <div
+              ref={dropdownRef}
+              id="dropdown"
+              className={`z-10 ${
+                showdropdown ? "opacity-100" : "opacity-0 invisible"
+              } absolute left-[15px] top-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
+            >
+              <ul
+                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li>
+                  <Link
+                    href="/dashboard"
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Edit Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={`/Member/${session.user.name}`}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    As a Member
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={`/Creator/${session.user.name}`}
+                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    As a Creator
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
+        {session && (
+          <button
+            className="text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 "
+            onClick={() => {
+              signOut({ callbackUrl: "/" });
+            }}
+          >
+            Logout
           </button>
-
-          <div ref={dropdownRef} id="dropdown" className={`z-10 ${showdropdown ? "opacity-100" : "opacity-0 invisible"} absolute left-[15px] top-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-              <li>
-                <Link href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit Profile</Link>
-              </li>
-              <li>
-                <Link href={`/Member/${session.user.name}`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">As a Member</Link>
-              </li>
-              <li>
-                <Link href={`/Creator/${session.user.name}`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">As a Creator</Link>
-              </li>
-              
-            </ul>
-          </div></>
-        }
-        {session && <button className='text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={() => { signOut({"callbackUrl": '/'}) }}>Logout</button>}
-        {!session && <Link href={"/login"}>
-          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2'>Login</button></Link>}
+        )}
+        {!session && (
+          <Link href={"/login"}>
+            
+            <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ">
+              Login
+            </button>
+          </Link>
+        )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
